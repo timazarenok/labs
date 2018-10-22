b3f4173c80d8894dd6a941238068217fdc4a8fe7
timofey <timofey.zarenok@gmail.com>
2018-10-22 22:07:17 +0300

    22.10.2018

--- project1/skypelesson3/Predicates.h
+++ project1/skypelesson3/Predicates.h
@@ -11,5 +11,10 @@ bool is_negative(int a)
     return !is_positive(a);
 }
 
+bool is_one(int a)
+{
+    return abs(a) == 1;
+}
+
 
 #endif // PREDICATES_H_INCLUDED
--- project1/skypelesson3/main.cpp
+++ project1/skypelesson3/main.cpp
@@ -6,12 +6,28 @@
 using namespace std;
 
 
+
 void print_array(int* arr, int size)
 {
     for(int i = 0; i < size; ++i)
     {
         cout<< arr[i] << " ";
     }
+}
+
+int* task3(int* arr, int size)
+{
+    int j = 0;
+    int* result = new int[size];
+    for(int i = 0; i < size; i+=2, ++j)
+    {
+        result[j] = arr[i];
+    }
+    for(int i = 1; i < size; i+=2, ++j)
+    {
+        result[j] = arr[i];
+    }
+    return result;
 }
 
 int* get_negative_elements(int* arr, int size)
@@ -21,6 +37,7 @@ int* get_negative_elements(int* arr, int size)
     copy_if(result, arr, size, is_negative);
     return result;
 }
+
 int* get_positive_elements(int* arr, int size)
 {
     int temp = count_if(arr, size, is_positive);
@@ -29,19 +46,33 @@ int* get_positive_elements(int* arr, int size)
     return result1;
 }
 
+void change_elements(int* arr, int size, UnaryPredicate p, int value)
+{
+    for(int i = 0; i < size; ++i)
+    {
+        if(p(arr[i]))
+        {
+            arr[i] = value;
+        }
+    }
+}
 
 int main()
 {
     int arr[10] {3, 2, -3, 2, 5, 1, 1, -1, -5, -2};
     int arr1[10]{0};
-    int* result1 = get_negative_elements(arr, 10);
-    print_array(result1, 4);
-    delete[] result1;
+    int* result3 = task3(arr, 10);
+    print_array(result3, 10);
     /*copy_if(arr1, arr, 10, is_positive);
     print_array(arr1, 10);
     cout<< " last positive argument = " << find_if(arr, 10, is_positive) <<endl;
     int result = accumulate(arr, arr + find_if(arr, 10, is_positive), 0);
     cout<< " sum = " << result <<endl;
+    int* result1 = get_negative_elements(arr, 10);
+    print_array(result1, 4);
+    delete[] result1;
+    change_elements(arr, 10, is_one, 10);
+    print_array(arr, 10);
     */
     return 0;
 }
